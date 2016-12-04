f50cf2f806700ec58ed45592b380305315f555dd
rebcabin <bc.beckman@gmail.com>
2016-12-03 17:00:15 -0800

    Deploying updates to clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 ;;; Copyright © 2017 Reb Cabin
 ;;; Distributed under The MIT License (MIT)
 
-(defproject funcyard "0.1.0-SNAPSHOT"
+(defproject funcyard "0.1.1-SNAPSHOT"
   :description "A scrap-heap of generally useful oddments."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/rebcabin/funcyard"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
                  [org.flatland/useful    "0.11.5"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/test.check "0.9.0"]]
+  )
