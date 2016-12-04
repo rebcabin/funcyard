1f1f518c0746479618dbabc68e82af8171d960fa
rebcabin <bc.beckman@gmail.com>
2016-12-03 16:39:15 -0800

    Added transpose, permutations, combinations.

--- src/funcyard/core.clj
+++ src/funcyard/core.clj
@@ -1,17 +1,68 @@
 ;;; Copyright © 2017 Reb Cabin
 ;;; Distributed under The MIT License (MIT)
 
-(ns funcyard.core)
+(ns funcyard.core
+  (:require clojure.pprint))
 
 (defn- inc-from-nil [v]
   (if (nil? v), 1, (inc v)))
 
 (defn tally [items]
   "Produces a map with keys from items and values that count the number of times
-  each item appears in the input. Valid only for sequences."
+  each item appears in the input collection."
   (reduce
    (fn [talmap item]
      (into talmap
            {item (inc-from-nil (talmap item))}))
    {} items))
+
+(defn pairwise [binary-function items]
+  "Zips the binary function down the a copy of the items with the first element
+  removed and the items. Useful for differences, piecewise geospatial distances,
+  numerical derivatives, and the like. \"Items\" must contain at least two
+  elements."
+  (when (and (first items) (second items))
+    (map binary-function (drop 1 items) items)))
+
+(defn transpose
+  "Produces the transpose of the given two-dimensional collection of
+  collections. A matrix is any collection on which \"get-in\" with a
+  two-argument sequence of keys would work."
+  [matrix]
+  (apply map list matrix))
+
+;;               _    _           _   _
+;;  __ ___ _ __ | |__(_)_ _  __ _| |_(_)___ _ _  ___
+;; / _/ _ \ '  \| '_ \ | ' \/ _` |  _| / _ \ ' \(_-<
+;; \__\___/_|_|_|_.__/_|_||_\__,_|\__|_\___/_||_/__/
+
+(defn combinations [xs m]
+  (cond
+    (= m 0) (list ())
+    (empty? (seq xs)) ()
+    :else (let [x (first xs)
+                xs (rest xs)]
+            (concat
+             (map #(cons x %) (combinations xs (- m 1)))
+             (combinations xs m)))))
+
+;;                          _        _   _
+;;  _ __  ___ _ _ _ __ _  _| |_ __ _| |_(_)___ _ _  ___
+;; | '_ \/ -_) '_| '  \ || |  _/ _` |  _| / _ \ ' \(_-<
+;; | .__/\___|_| |_|_|_\_,_|\__\__,_|\__|_\___/_||_/__/
+;; |_|
+
+(defn permutations [l]
+  (let [c (count l)]
+   (if (or (= c 0) (= c 1)),
+     (seq [l]),
+     (let [splits (map #(split-at % l) (range c))
+           pivots (map (comp first second) splits)
+           pres   (map first splits)
+           posts  (map (comp rest second) splits)
+           resids (map concat pres posts)
+           subps  (map permutations resids)
+           reasm  (mapcat #(map (partial cons %1) %2) pivots subps)]
+       reasm
+       ))))
 
--- test/funcyard/core_test.clj
+++ test/funcyard/core_test.clj
@@ -2,9 +2,11 @@
 ;;; Distributed under The MIT License (MIT)
 
 (ns funcyard.core-test
-  (:require [clojure.test       :refer :all]
-            [flatland.useful.fn :as    flatland]
-            [funcyard.core      :refer :all]))
+  (:require
+   clojure.pprint
+   [clojure.test       :refer :all]
+   [flatland.useful.fn :as    flatland]
+   [funcyard.core      :refer :all]))
 
 ;;  _        _ _
 ;; | |_ __ _| | |_  _
@@ -31,3 +33,106 @@
   (testing "tally on empty set"
     (is (= {}
            (tally #{})))))
+
+;;             _            _
+;;  _ __  __ _(_)_ ___ __ _(_)___ ___
+;; | '_ \/ _` | | '_\ V  V / (_-</ -_)
+;; | .__/\__,_|_|_|  \_/\_/|_/__/\___|
+;; |_|
+
+(deftest pairwise-on-squares
+  (testing "pairwise on list of squares"
+    (is (= [5 7 9]
+           (pairwise - [4 9 16 25])))))
+
+(deftest pairwise-on-short-things
+  (testing "pairwise should produce nil if there are fewer than 2 items."
+    (is (nil? (pairwise - [42])))
+    (is (nil? (pairwise - [0])))
+    (is (nil? (pairwise - [])))
+    (is (nil? (pairwise - '(42))))
+    (is (nil? (pairwise - '())))
+    (is (nil? (pairwise - #{42})))
+    (is (nil? (pairwise - #{})))))
+
+(deftest pairwise-expect-divide-by-zero
+  (testing "pairwise divides in the correct direction."
+    (is (thrown? java.lang.ArithmeticException
+                 (doall (pairwise / [0 1]))))))
+
+;;  _
+;; | |_ _ _ __ _ _ _  ____ __  ___ ___ ___
+;; |  _| '_/ _` | ' \(_-< '_ \/ _ (_-</ -_)
+;;  \__|_| \__,_|_||_/__/ .__/\___/__/\___|
+;;                      |_|
+
+(deftest transpose-vector-of-vectors
+  (testing "transpose on vector of vectors"
+    (is (= [[1 3] [2 4]]
+           (transpose [[1 2] [3 4]])))))
+
+(deftest transpose-of-singleton
+  (testing "transpose of a 1-by-1 matrix"
+    (is (= '((1))
+           (transpose [[1]])))))
+
+(deftest transpose-of-empty
+  (testing "transpose of an empty structure"
+    (is (= '()
+           (transpose [[]])))))
+
+;;               _    _           _   _
+;;  __ ___ _ __ | |__(_)_ _  __ _| |_(_)___ _ _  ___
+;; / _/ _ \ '  \| '_ \ | ' \/ _` |  _| / _ \ ' \(_-<
+;; \__\___/_|_|_|_.__/_|_||_\__,_|\__|_\___/_||_/__/
+
+(deftest combinations-basic
+  (testing "combinations"
+    (is (= '((a b) (a c) (a d) (b c) (b d) (c d))
+           (combinations '(a b c d) 2))
+        (= (partition 1 '(a b))
+           (combinations '(a b) 1)))))
+
+;;                          _        _   _
+;;  _ __  ___ _ _ _ __ _  _| |_ __ _| |_(_)___ _ _  ___
+;; | '_ \/ -_) '_| '  \ || |  _/ _` |  _| / _ \ ' \(_-<
+;; | .__/\___|_| |_|_|_\_,_|\__\__,_|\__|_\___/_||_/__/
+;; |_|
+
+(deftest permutations-basic
+  (testing "basics of permutations"
+    (is (= (permutations ['a 'b 'c 'd])
+           '((a b c d)
+             (a b d c)
+             (a c b d)
+             (a c d b)
+             (a d b c)
+             (a d c b)
+             (b a c d)
+             (b a d c)
+             (b c a d)
+             (b c d a)
+             (b d a c)
+             (b d c a)
+             (c a b d)
+             (c a d b)
+             (c b a d)
+             (c b d a)
+             (c d a b)
+             (c d b a)
+             (d a b c)
+             (d a c b)
+             (d b a c)
+             (d b c a)
+             (d c a b)
+             (d c b a))))))
+
+(deftest testing-permutation-singleton
+  (is (= '((a))
+         (permutations '(a)))))
+
+(deftest transpose-of-permutations
+  (is (= '((a a b b c c)
+           (b c a c a b)
+           (c b c a b a))
+         (transpose (permutations '(a b c))))))
